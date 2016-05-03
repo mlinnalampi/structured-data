f61eeecc27c240e7fdf3a6a3ad26324b697ac183
Markus Linnalampi <markus.linnalampi@gmail.com>
2016-05-03 20:59:11 +0300

    Half way there

--- src/structured_data.clj
+++ src/structured_data.clj
@@ -1,16 +1,18 @@
 (ns structured-data)
 
 (defn do-a-thing [x]
-  :-)
+  (let [a (+ x x)]
+	(Math/pow a a)))
 
 (defn spiff [v]
-  :-)
+  (+ (get v 0) (get v 2)))
 
 (defn cutify [v]
-  :-)
+  (conj v "<3"))
 
 (defn spiff-destructuring [v]
-  :-)
+  (let [[x a y] v]
+	(+ x y)))
 
 (defn point [x y]
   [x y])
@@ -19,22 +21,42 @@
   [bottom-left top-right])
 
 (defn width [rectangle]
-  :-)
+  (let [[[x1 y1] [x2 y2]] rectangle]
+    (- x2 x1)))
 
 (defn height [rectangle]
-  :-)
+  (let [[[x1 y1] [x2 y2]] rectangle]
+    (- y2 y1)))
 
 (defn square? [rectangle]
-  :-)
+  (== (height rectangle) (width rectangle)))
 
 (defn area [rectangle]
-  :-)
+  (* (height rectangle) (width rectangle)))
 
 (defn contains-point? [rectangle point]
-  :-)
+  (let [[[x1 y1] [x2 y2]] rectangle]
+    (let [[x y] point]
+      (and 
+        (<= x1 x x2) 
+        (<= y1 y y2)
+      )
+    )
+  )
+)
 
 (defn contains-rectangle? [outer inner]
-  :-)
+  (
+    let [[a b] inner]
+
+    (and
+      (contains-point? outer a)
+      (contains-point? outer b)
+      )
+
+    )
+
+  )
 
 (defn title-length [book]
   :-)
